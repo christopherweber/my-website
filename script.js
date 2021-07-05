ca89b3634dd8156c227268730865be0f54bd58dd
Chris Weber <chrisrrweber@gmail.com>
2021-07-05 16:55:25 -0400

    added images

--- script.js
+++ script.js
@@ -1,59 +1,43 @@
-const galleryItem = document.getElementsByClassName("gallery-item");
-const lightBoxContainer = document.createElement("div");
-const lightBoxContent = document.createElement("div");
-const lightBoxImg = document.createElement("img");
-const lightBoxPrev = document.createElement("div");
-const lightBoxNext = document.createElement("div");
+$(window).on('load',function(){	//画面遷移時にギャラリーの画像が被らないように、すべての読み込みが終わった後に実行する
 
-lightBoxContainer.classList.add("lightbox");
-lightBoxContent.classList.add("lightbox-content");
-lightBoxPrev.classList.add("fa", "fa-angle-left", "lightbox-prev");
-lightBoxNext.classList.add("fa", "fa-angle-right", "lightbox-next");
-
-lightBoxContainer.appendChild(lightBoxContent);
-lightBoxContent.appendChild(lightBoxImg);
-lightBoxContent.appendChild(lightBoxPrev);
-lightBoxContent.appendChild(lightBoxNext);
-
-document.body.appendChild(lightBoxContainer);
-
-let index = 1;
-
-function showLightBox(n) {
-    if (n > galleryItem.length) {
-        index = 1;
-    } else if (n < 1) {
-        index = galleryItem.length;
-    }
-    let imageLocation = galleryItem[index-1].children[0].getAttribute("src");
-    lightBoxImg.setAttribute("src", imageLocation);
-}
-
-function currentImage() {
-    lightBoxContainer.style.display = "block";
-
-    let imageIndex = parseInt(this.getAttribute("data-index"));
-    showLightBox(index = imageIndex);
-}
-for (let i = 0; i < galleryItem.length; i++) {
-    galleryItem[i].addEventListener("click", currentImage);
-}
-
-function slideImage(n) {
-    showLightBox(index += n);
-}
-function prevImage() {
-    slideImage(-1);
-}
-function nextImage() {
-    slideImage(1);
-}
-lightBoxPrev.addEventListener("click", prevImage);
-lightBoxNext.addEventListener("click", nextImage);
-
-function closeLightBox() {
-    if (this === event.target) {
-        lightBoxContainer.style.display = "none";
-    }
-}
-lightBoxContainer.addEventListener("click", closeLightBox);
+    //＝＝＝Muuriギャラリープラグイン設定
+    var grid = new Muuri('.grid', {
+    
+    //アイテムの表示速度※オプション。入れなくても動作します
+    showDuration: 600,
+    showEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+    hideDuration: 600,
+    hideEasing: 'cubic-bezier(0.215, 0.61, 0.355, 1)',
+        
+    // アイテムの表示/非表示状態のスタイル※オプション。入れなくても動作します
+      visibleStyles: {
+        opacity: '1',
+        transform: 'scale(1)'
+      },
+      hiddenStyles: {
+        opacity: '0',
+        transform: 'scale(0.5)'
+      }    
+    });
+    
+    //＝＝＝並び替えボタン設定
+    $('.sort-btn li').on('click',function(){			//並び替えボタンをクリックしたら
+        $(".sort-btn .active").removeClass("active");	//並び替えボタンに付与されているactiveクラスを全て取り除き
+        var className = $(this).attr("class");			//クラス名を取得
+        className = className.split(' ');				//「sortXX active」のクラス名を分割して配列にする
+        $("."+className[0]).addClass("active");			//並び替えボタンに付与されているクラス名とギャラリー内のリストのクラス名が同じボタンにactiveクラスを付与
+        if(className[0] == "sort00"){						//クラス名がsort00（全て）のボタンの場合は、
+             grid.show('');								//全ての要素を出す
+        }else{											//それ以外の場合は
+            grid.filter("."+className[0]); 				//フィルターを実行
+        }
+    });
+    
+    //＝＝＝ Fancyboxの設定
+    $('[data-fancybox]').fancybox({
+     thumbs: {
+        autoStart: true, //グループのサムネイル一覧をデフォルトで出す。不必要であればfalseに
+      },	
+    });
+        
+    });
